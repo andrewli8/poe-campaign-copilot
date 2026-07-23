085fa15537af621ac957904382eaa424e674c83c
andrewli8 <lia210350@gmail.com>
2026-07-22 23:47:07 -0400

    feat: register updater and process plugins with signing config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -418,6 +418,8 @@ fn main() {
                 .build(),
         )
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             get_model,
             set_setup_mode,
